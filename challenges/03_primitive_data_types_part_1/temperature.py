1cc22e82e6985a4c68952a07945b79321d46a4db
Q-debug410 <thethompsons410@hotmail.com>
2020-09-28 17:07:45 -0400

    adding temperature.p challenge

--- challenges/03_primitive_data_types_part_1/temperature.py
+++ challenges/03_primitive_data_types_part_1/temperature.py
@@ -1,2 +1,15 @@
-# # celsius_100
+# # formula to convert farenheit to celsius
+celsius_100 = (100-32)*0.55
+
 print(celsius_100)
+
+celsius_0 = (0-32)*0.55
+
+print(celsius_0)
+
+print((32.4-32)*0.55)
+
+celsius_5 = (5/.55)+32
+
+celsius_temp = (30.2/.55)+32
+print(celsius_temp)
